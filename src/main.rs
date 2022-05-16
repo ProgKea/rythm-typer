fc3d43f34c3f98236c147bb6bcc8a251b2d4695a
ProgKea <hoschy01@googlemail.com>
2022-05-16 14:57:24 +0200

    added highlight character

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn main() {
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup)
         .insert_resource(ClearColor(Color::DARK_GRAY))
+        .insert_resource(TargetString(
+            reqwest::blocking::get("https://random-word-api.herokuapp.com/word?number=1")
+                .unwrap()
+                .text()
+                .unwrap(),
+        ))
         .insert_resource(WindowDescriptor {
             present_mode: bevy::window::PresentMode::Mailbox,
             ..default()
         })
         .add_system(write_text)
+        .add_system(highlight_character.before(write_text))
         .add_system(check_quit)
         .run();
 }
@@ -21,18 +28,20 @@ struct TargetText;
 #[derive(Component)]
 struct HighlightCharacter;
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-    let mut target_string =
-        reqwest::blocking::get("https://random-word-api.herokuapp.com/word?number=10")
-            .unwrap()
-            .text()
-            .unwrap();
+#[derive(Default)]
+struct TargetString(String);
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut target_string: ResMut<TargetString>,
+) {
     let removed_chars = vec!['[', ']', '"'];
 
     for char in removed_chars {
-        target_string = target_string.replace(char, "");
+        target_string.0 = target_string.0.replace(char, "");
     }
-    target_string = target_string.replace(',', " ");
+    target_string.0 = target_string.0.replace(',', " ");
 
     commands.spawn_bundle(UiCameraBundle::default());
     commands
@@ -48,7 +57,7 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ..default()
             },
             text: Text::with_section(
-                target_string.clone(),
+                target_string.0.clone(),
                 TextStyle {
                     font: asset_server.load("fonts/Ubuntu Mono.ttf"),
                     font_size: 50.0,
@@ -59,20 +68,59 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             ..default()
         })
         .insert(TargetText);
+
+    commands
+        .spawn_bundle(TextBundle {
+            style: Style {
+                align_self: AlignSelf::Center,
+                position_type: PositionType::Absolute,
+                position: Rect {
+                    bottom: Val::Percent(50.0),
+                    left: Val::Px(10.0),
+                    ..default()
+                },
+                ..default()
+            },
+            text: Text::with_section(
+                target_string.0.get(0..1).unwrap(),
+                TextStyle {
+                    font: asset_server.load("fonts/Ubuntu Mono.ttf"),
+                    font_size: 50.0,
+                    color: Color::GOLD,
+                },
+                Default::default(),
+            ),
+            ..default()
+        })
+        .insert(HighlightCharacter);
 }
 
 fn write_text(
     mut target_text: Query<&mut Text, With<TargetText>>,
+    mut target_string: ResMut<TargetString>,
     mut char_evr: EventReader<ReceivedCharacter>,
 ) {
     for ev in char_evr.iter() {
-        for mut text in target_text.iter_mut() {
-            if text.sections[0].value.clone().len() > 0 {
-                if text.sections[0].value.clone().chars().next().unwrap() == ev.char {
-                    text.sections[0].value =
-                        format!("{}", text.sections[0].value.clone().get(1..).unwrap());
-                }
+        if target_string.0.chars().next().unwrap() == ev.char {
+            if target_string.0.len() > 1 {
+                target_string.0 = target_string.0.clone().get(1..).unwrap().to_string();
+                target_text.single_mut().sections[0].value = target_string.0.clone();
             }
+            else {
+                // TODO: end the game and give option to restart
+                todo!();
+            }
+        }
+    }
+}
+
+fn highlight_character(
+    target_string: ResMut<TargetString>,
+    mut highlight_character: Query<&mut Text, With<HighlightCharacter>>,
+) {
+    if target_string.is_changed() {
+        for mut char in highlight_character.iter_mut() {
+            char.sections[0].value = target_string.0.get(0..1).unwrap().to_string();
         }
     }
 }
